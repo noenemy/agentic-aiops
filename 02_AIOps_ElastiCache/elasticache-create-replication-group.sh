6b782c38ed93be2f1658902ef0c8b0a991fe80de
yjwang <smile930602@gmail.com>
2025-07-23 16:27:21 +0900

    upload elasticache-create-replication-group.sh

---
+++ 02_AIOps_ElastiCache/elasticache-create-replication-group.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "테스트 클러스터를 생성합니다."
+
+aws elasticache create-replication-group --replication-group-id test-valkey-cluster \
+--replication-group-description 'q cli demo valkey cluster' \
+--cache-node-type cache.r5.2xlarge \
+--engine valkey \
+--cluster-mode Enabled \
+--no-transit-encryption-enabled \
+--at-rest-encryption-enabled \
+--num-node-groups 2 \
+--replicas-per-node-group 2 \
+--region ap-northeast-2
